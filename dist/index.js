d947e72bb8504f06a29ed8cc3ca0341750dbb085
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-02-25 18:12:37 +0700

    Tiép

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
 console.log(JSON.stringify(process.env));
-let GITHUB_secrets = JSON.parse(process.env.GITHUB_secrets);
+let GITHUB_secrets = process.env.GITHUB_secrets;
 console.log(Buffer.from(GITHUB_secrets.TEST01, "base64").toString("utf8"));
-console.log(Buffer.from(GITHUB_secrets.TEST01).toString("base64"));
+console.log(Buffer.from(GITHUB_secrets.TEST02).toString("base64"));
