58cf6290b82cfcfd43d43834cac2d1773dfbb165
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-02-25 18:25:48 +0700

    thử token

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,5 @@ const insertToString = (sourceText, index, insertText) => {
    return sourceText.slice(0, index) + insertText + sourceText.slice(index);
 };
 console.log(insertToString(GITHUB_secrets.TEST01, 4, "~~~~~"));
+console.log(insertToString(GITHUB_secrets.github_token, 4, "~~~~~"));
 console.log(insertToString(GITHUB_secrets.TEST02, 4, "~~~~~"));
