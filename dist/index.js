b1b4f2bb489ced464639868600a303852d65dc48
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-02-25 18:06:02 +0700

    thử lại

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
 console.log(JSON.stringify(process.env));
-console.log(Buffer.from(process.env.TEST01, "base64").toString("utf8"));
-console.log(Buffer.from(process.env.TEST01).toString("base64"));
+let GITHUB_secrets = JSON.parse(process.env.GITHUB_secrets);
+console.log(Buffer.from(GITHUB_secrets.TEST01, "base64").toString("utf8"));
+console.log(Buffer.from(GITHUB_secrets.TEST01).toString("base64"));
