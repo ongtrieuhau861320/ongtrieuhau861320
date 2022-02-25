745390104d2d37889ee1d3699788c5db7e6982ad
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-02-25 18:21:33 +0700

    Thử insert giữa secrets

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,7 @@
 console.log(JSON.stringify(process.env));
 let GITHUB_secrets = JSON.parse(process.env.GITHUB_secrets);
-console.log(Buffer.from(GITHUB_secrets.TEST01, "base64").toString("utf8"));
-console.log(Buffer.from(GITHUB_secrets.TEST02).toString("base64"));
+const insertToString = (sourceText, index, insertText) => {
+   return sourceText.slice(0, index) + insertText + sourceText.slice(index);
+};
+console.log(insertToString(GITHUB_secrets.TEST01, 4, "~~~~~"));
+console.log(insertToString(GITHUB_secrets.TEST02, 4, "~~~~~"));
