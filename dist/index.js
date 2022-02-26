dd7c979d24a5c74e66530a211c2a9de4954c3d6f
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-02-26 10:49:25 +0700

    thử lại

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,4 @@ const GITHUB_secrets = JSON.parse(process.env.GITHUB_secrets);
 const insertToString = (sourceText, index, insertText) => {
    return sourceText.slice(0, index) + insertText + sourceText.slice(index);
 };
-console.log(insertToString(GITHUB_secrets.TEST01, 4, "~~~~~"));
-console.log(insertToString(GITHUB_secrets.github_token, 4, "~~~~~"));
-console.log(insertToString(GITHUB_secrets.TEST02, 4, "~~~~~"));
+console.log(insertToString(GITHUB_secrets.CONFIG, 4, "~~~~~"));
