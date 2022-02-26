3d27e9cf292073d8fdfd6a1858d383082ca43170
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-02-26 10:54:26 +0700

    Tử nhé

--- dist/index.ps1
+++ dist/index.ps1
@@ -1,3 +1,5 @@
 # "$env:SUPER_SECRET"
 # $GITHUB_secrets = JSON.parse(process.env.GITHUB_secrets);
 dir env:
+$GITHUB_secrets = $env:GIGITHUB_secrets | ConvertFrom-Json
+Write-Output ($GITHUB_secrets | ConvertTo-Json)
