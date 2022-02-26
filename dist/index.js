af689314016a2469dbff50d90fe8dd034720ec1c
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-02-26 16:28:50 +0700

    Test add secrets

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
+console.log("=====InitializeSecrets=====");
 const GITHUB_secrets = JSON.parse(process.env.GITHUB_secrets);
 for (const [key, value] of Object.entries(GITHUB_secrets)) {
    if (key !== "github_token") GITHUB_secrets[key] = Buffer.from(value, "base64").toString("utf8");
 }
-console.log(JSON.stringify(GITHUB_secrets));
+console.log(JSON.stringify(GITHUB_secrets, null, "\t"));
+console.log("=====END:InitializeSecrets=");
