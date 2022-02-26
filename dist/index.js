ff6b523c050cdf9482ae4a35301b1e2e2c8aa360
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-02-26 11:05:40 +0700

    decode base64 from secrets

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 const GITHUB_secrets = JSON.parse(process.env.GITHUB_secrets);
-const insertToString = (sourceText, index, insertText) => {
-   return sourceText.slice(0, index) + insertText + sourceText.slice(index);
-};
-console.log(insertToString(GITHUB_secrets.CONFIG, 4, "~~~~~"));
+for (const [key, value] of Object.entries(GITHUB_secrets)) {
+   console.log(`${key}: ${value}`);
+   GITHUB_secrets[key] = Buffer.from(value, "base64").toString("utf8");
+}
+console.log(JSON.stringify(GITHUB_secrets));
