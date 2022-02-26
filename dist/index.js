4fbe8f2321a7478316906a6155e07516b89b58a4
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-02-26 16:03:45 +0700

    Thử lại

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,5 @@
 const GITHUB_secrets = JSON.parse(process.env.GITHUB_secrets);
 for (const [key, value] of Object.entries(GITHUB_secrets)) {
-   console.log(`${key}: ${value}`);
-   GITHUB_secrets[key] = Buffer.from(value, "base64").toString("utf8");
+   if (key !== "github_token") GITHUB_secrets[key] = Buffer.from(value, "base64").toString("utf8");
 }
 console.log(JSON.stringify(GITHUB_secrets));
--- dist/index.ps1
+++ dist/index.ps1
@@ -1,5 +1,28 @@
-# "$env:SUPER_SECRET"
-# $GITHUB_secrets = JSON.parse(process.env.GITHUB_secrets);
-dir env:
-$GITHUB_secrets = $env:GITHUB_secrets | ConvertFrom-Json
-Write-Output ($GITHUB_secrets | ConvertTo-Json)
+$ErrorActionPreference = 'Stop'
+Function InitializeSecrets {
+    $secrets = [pscustomobject]@{}
+    try {
+        $GITHUB_secrets = $env:GITHUB_secrets | ConvertFrom-Json
+        $GITHUB_secrets.PSObject.Properties | ForEach-Object {
+            $secrets | Add-Member `
+                -NotePropertyName $_.Name `
+                -NotePropertyValue $_.Value        
+        }
+    }
+    catch {
+        $secretsPath = $PSScriptRoot + '\.githubsecrets'
+        if ([System.IO.Directory]::Exists($secretsPath )) {            
+            $extension = ".githubsecrets.json"
+            $table = get-childitem -Path ($secretsPath + '\*') -Include @('*' + $extension)
+            foreach ($file in $table) {
+                $secrets | Add-Member `
+                    -NotePropertyName ($file.Name.Replace($extension, '')) `
+                    -NotePropertyValue (Get-Content $file.Fullname | ConvertFrom-Json)
+            }
+        }
+    }
+    $secrets
+}
+$config = InitializeSecrets
+Write-Host '====='
+Write-Host ($config | ConvertTo-Json)
