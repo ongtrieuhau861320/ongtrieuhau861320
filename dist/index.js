7cd9768e65bf06395d9a6d83677e387a8f923763
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-02-26 16:51:25 +0700

    Change path in yml

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,12 @@
 const InitializeSecrets = () => {
    console.log("=====InitializeSecrets=====");
-   const GITHUB_secrets = JSON.parse(process.env.GITHUB_secrets);
-   let secrets = {};
-   for (const [key, value] of Object.entries(GITHUB_secrets)) {
-      if (key !== "github_token") secrets[key] = JSON.parse(Buffer.from(value, "base64").toString("utf8"));
+   if ("GITHUB_secrets" in process.env.GITHUB_secrets) {
+      const GITHUB_secrets = JSON.parse(process.env.GITHUB_secrets);
+      let secrets = {};
+      for (const [key, value] of Object.entries(GITHUB_secrets)) {
+         if (key !== "github_token") secrets[key] = JSON.parse(Buffer.from(value, "base64").toString("utf8"));
+      }
+   } else {
    }
    console.log(JSON.stringify(secrets, null, "\t"));
    console.log("=====END:InitializeSecrets=");
