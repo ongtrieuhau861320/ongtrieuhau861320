58bb17d33632472769834e78f4a63715838705f1
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-02-26 18:00:06 +0700

    Test GetLibraryFile on ubuntu

--- source/index.ps1
+++ source/index.ps1
@@ -35,56 +35,62 @@ function GetLibraryFile ([string] $pathLibrary) {
     [System.IO.FileInfo]$infoFile = New-Object System.IO.FileInfo($pathLibrary);
     $o = [pscustomobject]@{}
     $o | Add-Member -NotePropertyName OriginalFilename -NotePropertyValue $infoFile.Name
-    # $o.FileVersion = $infoFile.VersionInfo.FileVersion
-    # $o.FileDescription = $infoFile.VersionInfo.FileDescription
-    # $o.FileLength = $infoFile.Length
-    # $o.IsExe = $infoFile.Name -match '.exe$'
-    # $o.FileTime = New-Object FileTime
-    # $o.FileTime.CreationTime = $infoFile.CreationTime.ToString("yyyyMMdd HH:mm:ss")
-    # $o.FileTime.LastWriteTime = $infoFile.LastWriteTime.ToString("yyyyMMdd HH:mm:ss")
-    # $o.FileTime.LastAccessTime = $infoFile.LastAccessTime.ToString("yyyyMMdd HH:mm:ss")
+    $o | Add-Member -NotePropertyName FileVersion -NotePropertyValue $infoFile.FileVersion
+    $o | Add-Member -NotePropertyName FileDescription -NotePropertyValue $infoFile.FileDescription
+    $o | Add-Member -NotePropertyName FileLength -NotePropertyValue $infoFile.FileLength
+    $o | Add-Member -NotePropertyName IsExe -NotePropertyValue ($infoFile.Name -match '.exe$')
+    $o | Add-Member -NotePropertyName FileTime -NotePropertyValue ([pscustomobject]@{})
+    $o.FileTime | Add-Member -NotePropertyName CreationTime `
+        -NotePropertyValue ($infoFile.CreationTime.ToString("yyyyMMdd HH:mm:ss"))
+    $o.FileTime | Add-Member -NotePropertyName LastWriteTime `
+        -NotePropertyValue ($infoFile.LastWriteTime.ToString("yyyyMMdd HH:mm:ss"))
+    $o.FileTime | Add-Member -NotePropertyName LastAccessTime `
+        -NotePropertyValue ($infoFile.LastAccessTime.ToString("yyyyMMdd HH:mm:ss"))
+    
+    $fileBytes = [System.IO.File]::ReadAllBytes($pathLibrary);
+    $o | Add-Member -NotePropertyName FileHashMD5 `
+        -NotePropertyValue ((Get-FileHash -InputStream  ([System.IO.MemoryStream]::New($fileBytes)) -Algorithm MD5).hash)
+    $o | Add-Member -NotePropertyName FileHashSHA1 `
+        -NotePropertyValue ((Get-FileHash -InputStream  ([System.IO.MemoryStream]::New($fileBytes)) -Algorithm SHA1).hash)
 		
-    # $fileBytes = [System.IO.File]::ReadAllBytes($pathLibrary);
-    # $o.FileHashMD5 = (Get-FileHash -InputStream  ([System.IO.MemoryStream]::New($fileBytes)) -Algorithm MD5).hash
-    # $o.FileHashSHA1 = (Get-FileHash -InputStream  ([System.IO.MemoryStream]::New($fileBytes)) -Algorithm SHA1).hash
-		
-    # try {
-    #     $assembly = [System.Reflection.Assembly]::Load($fileBytes)
-    #     $assemblyGetName = $assembly.GetName()
-    #     $assemblyFullName = $assembly.FullName
-    #     $o.AssemblyFullName = $assemblyFullName
-    #     $o.AssemblyFullNameMD5 = (Get-FileHash -Algorithm MD5 -InputStream ([System.IO.MemoryStream]::New([System.Text.Encoding]::ASCII.GetBytes($assemblyFullName)))).hash
-    #     $o.AssemblyFullNameSHA1 = (Get-FileHash -Algorithm SHA1 -InputStream ([System.IO.MemoryStream]::New([System.Text.Encoding]::ASCII.GetBytes($assemblyFullName)))).hash
+    try {
+        $assembly = [System.Reflection.Assembly]::Load($fileBytes)
+        $assemblyGetName = $assembly.GetName()
+        $assemblyFullName = $assembly.FullName
+        $o | Add-Member -NotePropertyName AssemblyFullName -NotePropertyValue $assemblyFullName
+        $o | Add-Member -NotePropertyName AssemblyFullNameMD5 -NotePropertyValue `
+        (Get-FileHash -Algorithm MD5 -InputStream ([System.IO.MemoryStream]::New([System.Text.Encoding]::ASCII.GetBytes($assemblyFullName)))).hash
+        $o | Add-Member -NotePropertyName AssemblyFullNameSHA1 -NotePropertyValue `
+        (Get-FileHash -Algorithm SHA1 -InputStream ([System.IO.MemoryStream]::New([System.Text.Encoding]::ASCII.GetBytes($assemblyFullName)))).hash
+        
 			
-    #     $o.AssemblyName = $assemblyGetName.Name
-    #     $o.AssemblyVersion = $assemblyGetName.Version.ToString()
-    #     $o.AssemblyProcessorArchitecture = $assemblyGetName.ProcessorArchitecture.ToString()
-    #     $o.AssemblyImageRuntimeVersion = $assembly.ImageRuntimeVersion
-			
-    #     $o.ReferencedAssemblies = New-Object System.Collections.Generic.List[string]
-    #     Foreach ($asm in $assembly.GetReferencedAssemblies()) {
-    #         $asmFullname = $asm.ToString().ToLower()
-    #         $assemblySYSTEM = $asmFullname.StartsWith("mscorlib,".ToLower())
-    #         $assemblySYSTEM = $assemblySYSTEM -or $asmFullname.StartsWith("WindowsBase,".ToLower())
-    #         $assemblySYSTEM = $assemblySYSTEM -or $asmFullname.StartsWith("System,".ToLower())
-    #         $assemblySYSTEM = $assemblySYSTEM -or $asmFullname.StartsWith("System.".ToLower())
-    #         if ($assemblySYSTEM -eq $false) {
-    #             $o.ReferencedAssemblies.Add($asm.FullName.ToString())
-    #             $deployDirectory.ReferenceAssemblyNames.Add($o.AssemblyName)
-    #         }
-    #     }
-    # }
-    # catch {
-    #     $ignore = $_.ToString().Contains("Could not load file or assembly 'ChilkatDotNet4, Version=9.5.0.73, Culture=neutral")
-    #     $ignore = $ignore -or $_.ToString().Contains("bytes loaded from Anonymously Hosted DynamicMethods Assembly")
-    #     if ($ignore) { }
-    #     else {
-    #         Write-Host $o.OriginalFilename
-    #         Write-Host $_
-    #     }
-    # }
+        $o | Add-Member -NotePropertyName AssemblyName -NotePropertyValue $assemblyGetName.Name
+        $o | Add-Member -NotePropertyName AssemblyVersion -NotePropertyValue $assemblyGetName.Version.ToString()
+        $o | Add-Member -NotePropertyName AssemblyProcessorArchitecture -NotePropertyValue $assemblyGetName.ProcessorArchitecture.ToString()
+        $o | Add-Member -NotePropertyName AssemblyImageRuntimeVersion -NotePropertyValue $assembly.ImageRuntimeVersion
+        $o | Add-Member -NotePropertyName ReferencedAssemblies -NotePropertyValue (New-Object System.Collections.Generic.List[string])
+        Foreach ($asm in $assembly.GetReferencedAssemblies()) {
+            $asmFullname = $asm.ToString().ToLower()
+            $assemblySYSTEM = $asmFullname.StartsWith("mscorlib,".ToLower())
+            $assemblySYSTEM = $assemblySYSTEM -or $asmFullname.StartsWith("WindowsBase,".ToLower())
+            $assemblySYSTEM = $assemblySYSTEM -or $asmFullname.StartsWith("System,".ToLower())
+            $assemblySYSTEM = $assemblySYSTEM -or $asmFullname.StartsWith("System.".ToLower())
+            if ($assemblySYSTEM -eq $false) {
+                $o.ReferencedAssemblies.Add($asm.FullName.ToString())
+            }
+        }
+    }
+    catch {
+        $ignore = $_.ToString().Contains("Could not load file or assembly 'ChilkatDotNet4, Version=9.5.0.73, Culture=neutral")
+        $ignore = $ignore -or $_.ToString().Contains("bytes loaded from Anonymously Hosted DynamicMethods Assembly")
+        if ($ignore) { }
+        else {
+            Write-Host $o.OriginalFilename
+            Write-Host $_
+        }
+    }
     return $o
 }
 $secrets = InitializeSecrets
-$lib = GetLibraryFile -pathLibrary ($PSScriptRoot + '\Hex_Bytes.dll')
-Write-Host $lib
+$lib = GetLibraryFile -pathLibrary ($PSScriptRoot + '\OTH.PAY.ViettinBank.Crypto.dll')
+Write-Host ($lib | ConvertTo-Json)
