afce44e06cc2002acfbbf3883fd23dedb9167d8b
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-02-26 10:56:09 +0700

    Fix fail

--- dist/index.ps1
+++ dist/index.ps1
@@ -1,5 +1,5 @@
 # "$env:SUPER_SECRET"
 # $GITHUB_secrets = JSON.parse(process.env.GITHUB_secrets);
 dir env:
-$GITHUB_secrets = $env:GIGITHUB_secrets | ConvertFrom-Json
+$GITHUB_secrets = $env:GITHUB_secrets | ConvertFrom-Json
 Write-Output ($GITHUB_secrets | ConvertTo-Json)
